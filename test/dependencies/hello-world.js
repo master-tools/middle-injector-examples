a12ddde9f59384af0158acc3760f01cbed49dc91
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2017-01-06 18:16:44 +0100

    FIX(test/dependencies/hello-world.js): get middleware instance
    Add middleware instance to use it on test.

--- test/dependencies/hello-world.js
+++ test/dependencies/hello-world.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Get the instance of the middleware
+let helloWorld = require('../../middlewares/hello-world');
+
 // This example only have one dependence, it is just a hello world middleware that return that text.
 let dependencies = [
     {
@@ -8,4 +11,5 @@ let dependencies = [
     }
 ];
 
+//Export it to re use it in other test
 module.exports = dependencies;
